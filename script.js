136cf9faaf7c6a53b5926324b9bed893df25ca21
JISHIRO <kronosiraxx@gmail.com>
2024-12-10 19:13:18 -0600

    Primera Actualizacion

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por contactarnos! Nos pondremos en contacto contigo pronto.');
+});
